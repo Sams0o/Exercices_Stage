eec9317cc426db475cec5225696d6ee3d31bbda8
Sams0o <samra.mhoma@gmail.com>
2024-03-01 12:58:10 +0100

    Exercice1

---
+++ Exercice1.js
@@ -0,0 +1,14 @@
+const assert = require("assert");
+
+function categorize(values) {
+    const objetClés = {}
+
+    values.forEach(elementValue => {
+      let type = typeof elementValue;
+    });
+
+
+  return objetClés;
+}
+
+console.log(categorize());
