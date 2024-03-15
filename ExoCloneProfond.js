0d41ad8b5185a10d3d4c1fa238c533ae05f86db7
Sams0o <samra.mhoma@gmail.com>
2024-03-15 11:49:28 +0100

    Clone profond EN COURS

---
+++ ExoCloneProfond.js
@@ -0,0 +1,34 @@
+//Créer une fonction qui permet de produire un "clone profond" sur un objet passé en paramètre : 
+
+function deepClone(value){
+    if (typeof value !== "object" || value === null) {
+      return value;
+    } 
+
+    const newObject = {}
+
+    Object.entries(value).forEach(([key, value]) => {
+      newObject[key] = deepClone(value)
+    });
+
+    return newObject;
+      
+}
+ 
+const original = { 
+  n: 1, 
+  subObj: { 
+    s: 'foo' 
+  } 
+}
+ 
+const clone = deepClone(original)
+ 
+if ([
+  () => clone !== original,
+  () => clone.n === original.n,
+  () => typeof clone.subObj === 'object',
+  () => clone.subObj !== original.subObj,
+  () => clone.subObj.s === original.subObj.s
+].some(check => !check()))
+  throw Error('not a deep clone !')
